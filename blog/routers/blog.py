822fb564aaa0d79abff9699c11af4b3ef0bc8528
Shah rahman <toshah.rahman@gmail.com>
2022-05-16 17:54:15 +0300

    fast api fun v2 27

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, status, Depends
-from .. import databases, models as _models
+from .. import databases, models as _models, schemas as _schemas
 from sqlalchemy.orm import Session
+from typing import List
 
 router = APIRouter()
 
@@ -10,7 +11,7 @@ get_db = databases.get_db()
 @router.get(
     "/blog",
     tags=["Blog"],
-    # response_model=List[_schemas.ShowBlog],
+    response_model=List[_schemas.ShowBlog],
     status_code=status.HTTP_200_OK,
 )
 def all_blog(db: Session = Depends(get_db)):

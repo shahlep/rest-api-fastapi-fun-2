1b96e3b6a9c92597d848045e2a2cc09ac3065916
Shah rahman <toshah.rahman@gmail.com>
2022-05-17 18:02:25 +0300

    fast api fun v2 43

--- blog/hashing.py
+++ blog/hashing.py
@@ -7,3 +7,6 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 class Hash:
     def encrypt(password: str):
         return pwd_context.hash(password)
+
+    def verify(plain_password, hashed_password):
+        return pwd_context.verify(plain_password,hashed_password)
--- blog/routers/login.py
+++ blog/routers/login.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException,status
 from .. import schemas as _schemas, databases, models as _models
 from sqlalchemy.orm import Session
+from ..hashing import Hash
 
 router = APIRouter(tags=["User Authentication"])
 
@@ -11,5 +12,9 @@ def login(user_info: _schemas.login, db: Session = Depends(databases.get_db)):
     if not user:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f"Username don't exist!"
+        )
+    if not Hash.verify(user_info.password, _models.User.password):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid password!"
         )
     return user

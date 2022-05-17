392dea54ab64975f980293a89f2fae52aa3737a2
Shah rahman <toshah.rahman@gmail.com>
2022-05-17 16:43:12 +0300

    fast api fun v2 36

--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from . import models as _models
 from .databases import *
-from .routers import blog, user
+from .routers import blog, user, login
 
 app = FastAPI()
 
 app.include_router(blog.router)
 app.include_router(user.router)
+app.include_router(login.router)
 
 _models.Base.metadata.create_all(bind=engine)

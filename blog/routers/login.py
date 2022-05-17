115003ebdfeaf7e4a35a192f5c0b20dabdafb9dd
Shah rahman <toshah.rahman@gmail.com>
2022-05-17 17:49:05 +0300

    fast api fun v2 41

--- blog/routers/login.py
+++ blog/routers/login.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException,status
 from .. import schemas as _schemas, databases, models as _models
 from sqlalchemy.orm import Session
 
@@ -8,3 +8,8 @@ router = APIRouter(tags=["User Authentication"])
 @router.post("/login")
 def login(user_info: _schemas.login, db: Session = Depends(databases.get_db)):
     user = db.query(_models.User.email == user_info.username).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Username don't exist!"
+        )
+    return user

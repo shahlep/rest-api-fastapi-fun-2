257c867fbd99c3a12ef86086e285f3a6e6f84941
Shah rahman <toshah.rahman@gmail.com>
2022-05-17 17:46:24 +0300

    fast api fun v2 40

--- blog/routers/login.py
+++ blog/routers/login.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter
-from .. import schemas as _schemas
+from fastapi import APIRouter, Depends
+from .. import schemas as _schemas, databases, models as _models
+from sqlalchemy.orm import Session
 
 router = APIRouter(tags=["User Authentication"])
 
 
 @router.post("/login")
-def login(user_info: _schemas.login):
-    pass
+def login(user_info: _schemas.login, db: Session = Depends(databases.get_db)):
+    user = db.query(_models.User.email == user_info.username).first()

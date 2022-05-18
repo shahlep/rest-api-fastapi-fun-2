d389f813bfb447b7924527152a011e9a174a79b9
Shah rahman <toshah.rahman@gmail.com>
2022-05-18 17:12:56 +0300

    fast api fun v2 48

--- blog/routers/login.py
+++ blog/routers/login.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from .. import schemas as _schemas, databases, models as _models
 from sqlalchemy.orm import Session
 from ..hashing import Hash
+from ..jwttoken import *
+from datetime import datetime, timedelta
 
 router = APIRouter(tags=["User Authentication"])
 
@@ -19,4 +21,6 @@ def login(user_info: _schemas.Login, db: Session = Depends(databases.get_db)):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid password!"
         )
-    return user
+    #return user
+    access_token = create_access_token(data={"sub": user.email})
+    return {"access_token": access_token, "token_type": "bearer"}

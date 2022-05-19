decbbd1db3be5d7c9dc95a981b2a4649d970fe0d
Shah rahman <toshah.rahman@gmail.com>
2022-05-19 06:55:20 +0300

    fast api fun v2 52

--- blog/routers/login.py
+++ blog/routers/login.py
@@ -9,9 +9,7 @@ router = APIRouter(tags=["User Authentication"])
 
 @router.post("/login")
 def login(user_info: _schemas.User, db: Session = Depends(databases.get_db)):
-    user = (
-        db.query(_models.User).filter(_models.User.email == user_info.email).first()
-    )
+    user = db.query(_models.User).filter(_models.User.email == user_info.email).first()
     if not user:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f"Username don't exist!"
